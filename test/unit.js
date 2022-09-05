e56e96fa90c7be8aaeb11fe1b888b31efde54fad
Zbynek Roubalik <zroubalik@gmail.com>
2022-09-05 10:29:21 +0200

    init
    Signed-off-by: Zbynek Roubalik <zroubalik@gmail.com>

---
+++ test/unit.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const func = require('..').handle;
+const test = require('tape');
+
+const fixture = { log: { info: console.log } };
+
+test('Unit: handles an HTTP GET', async t => {
+  t.plan(1);
+  // Invoke the function, which should complete without error.
+  const result = await func({ ...fixture, method: 'GET', query: { name: 'tiger' } });
+  t.deepEqual(result, { query: { name: 'tiger' } });
+  t.end();
+});
+
+test('Unit: handles an HTTP POST', async t => {
+  t.plan(1);
+  // Invoke the function, which should complete without error.
+  const result = await func({ ...fixture, method: 'POST', body: { name: 'tiger' } });
+  t.deepEqual(result, { body: { name: 'tiger' } });
+  t.end();
+});
+
+test('Unit: responds with error code if neither GET or POST', async t => {
+  t.plan(1);
+  const result = await func(fixture);
+  t.deepEqual(result, { statusCode: 405, statusMessage: 'Method not allowed' });
+  t.end();
+});
